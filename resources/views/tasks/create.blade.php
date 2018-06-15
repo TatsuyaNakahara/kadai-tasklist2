1fcd3f1e8c335f6c305f22ba93ca050148c5a602
TatsuyaNakahara <tatsuya.nakahara@rakuten.com>
2018-06-15 06:43:41 +0000

    bootstrap design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -6,13 +6,18 @@
 
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
     
-        {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
+        <div class="form-group">
+                    {!! Form::label('status', 'ステータス:') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
+                </div>
+                    
+                <div class="form-group">
+                    {!! Form::label('content', 'タスク:') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
+                
+                {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
 
-        {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
-
-        {!! Form::submit('投稿') !!}
 
     {!! Form::close() !!}
 
